b82947aab0da4d607ea3ba12c1b41898957d6ad2
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-28 00:38:35 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,14 +1,2 @@
 class Student:
-    def __init__(self, name):
-        self.name = name
-        self.__grade = 0   # private attribute
-
-    def set_grade(self, val): 
-        # only allow values between 0 and 100
-        if 0 <= val <= 100:
-            self.__grade = val
-        else:
-            self.__grade = 0   # reset to 0 if invalid
-
-    def get_grade(self):
-        return self.__grade
+    pass
