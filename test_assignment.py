c95ff724f7a948044e03bf4b61a5b52b39b2458d
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-30 21:54:05 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -3,17 +3,10 @@ from assignment import Student
 
 def test1():
     s = Student("Bob")
-    s.set_grade(90)
-    assert s.get_grade() == 90
-
-@pytest.mark.parametrize("grade", [-1, 150, 999])
-def test2(grade):
-    s = Student("Test")
-    s.set_grade(grade)
-    assert s.get_grade() == 0  
-    
-def test3():
-    s = Student("A")
-    s.set_grade(50)
-    s.set_grade(70)
-    assert s.get_grade() == 70
+    s.set_grade(85)
+    assert s.get_grade() == 85
+    s.set_grade(150)
+    assert s.get_grade() == 0
+    with pytest.raises(AttributeError):
+        _ = s.__grade
+    assert s.name == "Bob"
